60b30ed45b5c2eb78c1d7bb5ddaa6b7c544e1fb5
Nicolas Oelgart <nico@nicoswd.com>
2017-10-23 23:22:20 +0200

    Cleanup

--- src/nicoSWD/Rules/EvaluatorInterface.php
+++ src/nicoSWD/Rules/EvaluatorInterface.php
@@ -5,11 +5,9 @@
  * @link        https://github.com/nicoSWD
  * @author      Nicolas Oelgart <nico@oelgart.com>
  */
-declare(strict_types=1);
-
 namespace nicoSWD\Rules;
 
 interface EvaluatorInterface
 {
-    public function evaluate(string $group) : bool;
+    public function evaluate(string $group): bool;
 }
