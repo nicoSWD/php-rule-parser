c42d980469e20669e047a77c3900204ae3dec9b6
Nicolas Oelgart <nico@nicoswd.com>
2017-11-06 23:33:48 +0100

    Cleanup

--- tests/integration/AbstractTestBase.php
+++ tests/integration/AbstractTestBase.php
@@ -9,40 +9,13 @@ declare(strict_types=1);
  */
 namespace nicoSWD\Rule\tests\integration;
 
-use nicoSWD\Rule\TokenStream\AST;
-use nicoSWD\Rule\Compiler\CompilerFactory;
-use nicoSWD\Rule\Evaluator\Evaluator;
-use nicoSWD\Rule\Grammar\JavaScript\JavaScript;
-use nicoSWD\Rule\Parser\Parser;
-use nicoSWD\Rule\Expression\ExpressionFactory;
-use nicoSWD\Rule\Tokenizer\Tokenizer;
-use nicoSWD\Rule\TokenStream\Token\TokenFactory;
-use nicoSWD\Rule\TokenStream\TokenStream;
+use nicoSWD\Rule\Rule;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestBase extends TestCase
 {
-    /** @var Parser */
-    protected $parser;
-    /** @var Evaluator */
-    protected $evaluator;
-    /** @var AST */
-    protected $ast;
-
-    final protected function setUp()
-    {
-        $tokenFactory = new TokenFactory();
-
-        $this->ast = new AST(new Tokenizer(new JavaScript(), $tokenFactory), $tokenFactory, new TokenStream());
-        $this->parser = new Parser($this->ast, new ExpressionFactory(), new CompilerFactory());
-        $this->evaluator = new Evaluator();
-    }
-
     protected function evaluate(string $rule, array $variables = []): bool
     {
-        $this->ast->setVariables($variables);
-        $result = $this->parser->parse($rule);
-
-        return $this->evaluator->evaluate($result);
+        return (new Rule($rule, $variables))->isTrue();
     }
 }
