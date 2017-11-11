7a1ec596e82805dfa1865a7f82ff13cded7d204c
Nicolas Oelgart <nico@nicoswd.com>
2017-11-11 22:49:35 +0100

    Call me crazy, one more time...

--- src/nicoSWD/Rule/Parser/Parser.php
+++ src/nicoSWD/Rule/Parser/Parser.php
@@ -9,12 +9,14 @@ declare(strict_types=1);
  */
 namespace nicoSWD\Rule\Parser;
 
+use Closure;
 use nicoSWD\Rule\Compiler\CompilerInterface;
 use nicoSWD\Rule\Compiler\CompilerFactoryInterface;
 use nicoSWD\Rule\Compiler\Exception\MissingOperatorException;
 use nicoSWD\Rule\Expression\ExpressionFactoryInterface;
 use nicoSWD\Rule\TokenStream\AST;
 use nicoSWD\Rule\TokenStream\Token\BaseToken;
+use nicoSWD\Rule\TokenStream\Token\TokenType;
 use SplStack;
 
 class Parser
@@ -27,6 +29,8 @@ class Parser
     private $compilerFactory;
     /** @var null|BaseToken */
     private $operator = null;
+    /** @var SplStack */
+    private $values;
 
     public function __construct(
         AST $ast,
@@ -42,47 +46,44 @@ class Parser
     {
         $compiler = $this->compilerFactory->create();
         $this->operator = null;
-        $values = new SplStack();
+        $this->values = new SplStack();
 
         foreach ($this->ast->getStream($rule) as $token) {
-            if ($token->isValue()) {
-                $values->push($token->getValue());
-            } elseif ($token->isWhitespace()) {
-                continue;
-            } elseif ($token->isOperator()) {
-                $this->assignOperator($token, $values);
-            } elseif ($token->isLogical()) {
-                $compiler->addLogical($token);
-            } elseif ($token->isParenthesis()) {
-                $compiler->addParentheses($token);
-            } else {
-                throw Exception\ParserException::unknownToken($token);
-            }
-
-            $this->evaluateExpression($values, $compiler);
+            $handler = $this->getTokenHandler($token);
+            $handler($compiler);
         }
 
         return $compiler->getCompiledRule();
     }
 
-    private function assignOperator(BaseToken $token, SplStack $values)
+    private function getTokenHandler(BaseToken $token): Closure
     {
-        if (isset($this->operator)) {
-            throw Exception\ParserException::unexpectedToken($token);
-        } elseif ($values->isEmpty()) {
-            throw Exception\ParserException::incompleteExpression($token);
-        }
+        return function (CompilerInterface $compiler) use ($token) {
+            $handlers = [
+                TokenType::VALUE       => $this->valueHandler(),
+                TokenType::INT_VALUE   => $this->valueHandler(),
+                TokenType::OPERATOR    => $this->operatorHandler(),
+                TokenType::LOGICAL     => $this->logicalHandler(),
+                TokenType::PARENTHESIS => $this->parenthesisHandler(),
+                TokenType::SPACE       => $this->dummyHandler(),
+                TokenType::COMMENT     => $this->dummyHandler(),
+                TokenType::UNKNOWN     => $this->unknownHandler(),
+            ];
 
-        $this->operator = $token;
+            $handler = $handlers[$token->getType()] ?? $handlers[TokenType::UNKNOWN];
+            $handler($compiler, $token);
+
+            $this->evaluateExpression($compiler);
+        };
     }
 
-    private function evaluateExpression(SplStack $values, CompilerInterface $compiler)
+    private function evaluateExpression(CompilerInterface $compiler)
     {
-        if (!isset($this->operator) || $values->count() !== 2) {
+        if (!isset($this->operator) || $this->values->count() !== 2) {
             return;
         }
 
-        list($rightValue, $leftValue) = $values;
+        list($rightValue, $leftValue) = $this->values;
 
         try {
             $expression = $this->expressionFactory->createFromOperator($this->operator);
@@ -95,9 +96,56 @@ class Parser
         }
 
         do {
-            $values->pop();
-        } while (!$values->isEmpty());
+            $this->values->pop();
+        } while (!$this->values->isEmpty());
 
         unset($this->operator);
+    }
+
+    private function valueHandler(): Closure
+    {
+        return function (CompilerInterface $compiler, BaseToken $token) {
+            $this->values->push($token->getValue());
+        };
+    }
+
+    private function operatorHandler(): Closure
+    {
+        return function (CompilerInterface $compiler, BaseToken $token) {
+            if (isset($this->operator)) {
+                throw Exception\ParserException::unexpectedToken($token);
+            } elseif ($this->values->isEmpty()) {
+                throw Exception\ParserException::incompleteExpression($token);
+            }
+
+            $this->operator = $token;
+        };
+    }
+
+    private function logicalHandler(): Closure
+    {
+        return function (CompilerInterface $compiler, BaseToken $token) {
+            $compiler->addLogical($token);
+        };
+    }
+
+    private function parenthesisHandler(): Closure
+    {
+        return function (CompilerInterface $compiler, BaseToken $token) {
+            $compiler->addParentheses($token);
+        };
+    }
+
+    private function unknownHandler(): Closure
+    {
+        return function (CompilerInterface $compiler, BaseToken $token) {
+            throw Exception\ParserException::unknownToken($token);
+        };
+    }
+
+    private function dummyHandler(): Closure
+    {
+        return function () {
+        };
     }
 }
